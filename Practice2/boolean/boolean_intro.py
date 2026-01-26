5e46c7407badde25310a9c66a118f694ebb0d72a
amiki075 <amirnurbaj6@gmail.com>
2026-01-26 13:26:58 +0500

    Add Practice2: booleans, if-else and loops examples

---
+++ Practice2/boolean/boolean_intro.py
@@ -0,0 +1,11 @@
+print(bool("Hello"))
+print(bool(15))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+#Booleans represent one of two values: True or False.
+
---
+++ Practice2/boolean/boolean_operators.py
@@ -0,0 +1,14 @@
+#And
+x = 5
+
+print(x > 0 and x < 10)
+
+#OR
+x = 5
+
+print(x < 5 or x > 10)
+
+#Not - reverse the result
+x = 5
+
+print(not(x > 3 and x < 10))
